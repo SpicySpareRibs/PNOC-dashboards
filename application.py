a24ca75ddb16477f0b3f47b1b520089a99ad7ddf
chrissenatin <cmsenatin@up.edu.ph>
2025-02-23 22:14:23 +0800

    Visually improved UI and added instructions

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, filedialog as fd
+from tkinter import ttk, font, filedialog as fd
 from typing import get_args
 
 from annual_procurement_plan_recompiler import AnnualProcurementPlanRecompiler
@@ -36,39 +36,71 @@ class GUI():
     def __init__(self):
         # Initialize the window
         self.root = tk.Tk()
-        self.root.title("Datafile Recompiler")
-        self.root.geometry("750x250")
+        self.root.title("PNOC Datafile Recompiler")
+        self.root.geometry("750x600")
         self.root.resizable(0,0)
 
+        self.default_font = font.nametofont("TkDefaultFont")
+        self.default_font.configure(family="Segoe UI", size=10, weight="normal")
+        self.text_font = font.nametofont("TkTextFont")
+        self.text_font.configure(family="Segoe UI", size=10, weight="normal")
+        self.bold_text_font = ["Segoe UI", 10, "bold"]
+
         # Initialize widgets
-        select_recompiler_label = ttk.Label(self.root, font = ("Times New Roman", 10))
-        select_recompiler_label['text'] = "Select the Recompiler Type :"
-        select_recompiler_label.grid(column = 0, row = 0, sticky='W') 
+        header_bg = tk.Canvas(self.root, width=750, height=115, bg='#991E20')
+        header_bg.create_line(0, 116, 750, 116, fill='#A6A6A6', width=2)
+        header_bg.place(x=0, y=0)
+        
+        footer = tk.Canvas(self.root, width=750, height=5)
+        footer.create_line(0,0,750,0, fill='#A6A6A6', width=5)
+        footer.place(x=160, y=515)
+
+        footer_text = tk.Label(self.root, font=["Segoe UI", 8, "normal"], foreground='#A6A6A6')
+        footer_text['text'] = "PNOC Datafile Recompiler"
+        footer_text.place(x=5, y=505)
+
+        title_text = ttk.Label(self.root, foreground='#FFF9E9', font=["Segoe UI", 10, "bold"], background='#991E20')
+        title_text['text'] = "Information"
+        title_text.place(x=40, y=20)
+
+        information_text = ttk.Label(self.root, wraplength=650, foreground='#FFF9E9', background='#991E20')
+        information_text['text'] = "This application recompiles the selected datafile into a Looker Studio compatible format. The output file can be located in the same folder as the application."
+        information_text.place(x=60, y=45)
+
+        select_recompiler_label = ttk.Label(self.root, font=self.bold_text_font)
+        select_recompiler_label['text'] = "Select Recompiler Type:"
+        select_recompiler_label.place(x=60, y=140)
 
         self.selected_recompiler = tk.StringVar()
-        select_recompiler_combobox = ttk.Combobox(self.root, width = 27, textvariable = self.selected_recompiler, postcommand = self.reset_status)
+        select_recompiler_combobox = ttk.Combobox(self.root, width = 27, textvariable = self.selected_recompiler, postcommand = self.reset_status, state='readonly')
         select_recompiler_combobox['values'] = get_args(filetype)
-        select_recompiler_combobox.grid(column = 1, row = 0, sticky='W')
+        select_recompiler_combobox.place(x=270, y=140)
         select_recompiler_combobox.current(0)
 
-        select_datafile_label = ttk.Label(self.root, font = ("Times New Roman", 10))
-        select_datafile_label['text'] = "Select the datafile: "
-        select_datafile_label.grid(column = 0, row = 1, sticky='W') 
+        recompiler_information = ttk.Label(self.root, wraplength=630)
+        recompiler_information['text'] = "The recompiler type is the type of file to be recompiled. The dropdown menu contains the supported types of files to be recompiled."
+        recompiler_information.place(x=80, y=175)
 
-        self.datafile_filepath = ""
-        select_datafile_button = ttk.Button(self.root, text="Browse", command=self.get_datafile)
-        select_datafile_button.grid(column = 1, row = 1, sticky='W')
+        select_datafile_label = ttk.Label(self.root, font=self.bold_text_font)
+        select_datafile_label['text'] = "Select the Input Datafile"
+        select_datafile_label.place(x=60, y=250)
 
-        self.datafile_filepath_label = ttk.Label(self.root, font = ("Times New Roman", 10), wraplength = 500, justify = 'left')
-        self.datafile_filepath_label['text'] = "Datafile Filepath: "
-        self.datafile_filepath_label.grid(column = 2, row = 1, sticky='W')
+        self.select_datafile_entry = ttk.Entry(self.root, width=53)
+        self.select_datafile_entry.place(x=80, y=285)
+
+        select_datafile_button = ttk.Button(self.root, text="Browse...", command=self.get_datafile)
+        select_datafile_button.place(x=580, y=283)
+
+        select_datafile_information = ttk.Label(self.root, wraplength=630)
+        select_datafile_information['text'] = "Locate on your device the specific datafile to be recompiled."
+        select_datafile_information.place(x=80, y=325)
 
         recompile_button = ttk.Button(self.root, text="Recompile", command=self.recompile)
-        recompile_button.grid(column = 1, row = 3, sticky='W')
+        recompile_button.place(x=580, y=540)
 
-        self.recompile_status = ttk.Label(self.root, font = ("Times New Roman", 10))
+        self.recompile_status = ttk.Label(self.root)
         self.recompile_status['text'] = ""
-        self.recompile_status.grid(column = 2, row = 3, sticky='W')
+        self.recompile_status.place(x=420, y=542)
 
         self.root.mainloop()
     
@@ -76,12 +108,11 @@ class GUI():
         self.recompile_status['text'] = ""
 
     def get_datafile(self) -> None:
-        self.datafile_filepath = fd.askopenfilename()
-        self.datafile_filepath_label['text'] = "Datafile Filepath: " + self.datafile_filepath
+        self.select_datafile_entry.insert(0, fd.askopenfilename())
         self.reset_status()
     
     def recompile(self) -> None:
-        datafile = Datafile(self.datafile_filepath, filetype=self.selected_recompiler.get())
+        datafile = Datafile(self.select_datafile_entry.get(), filetype=self.selected_recompiler.get())
         test_recompiler = RecompilerMaker.make(datafile)
         self.status_label_timer = 5
         try:
